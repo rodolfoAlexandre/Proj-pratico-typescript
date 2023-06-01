d4e21e969f5068b7b585b068ddab2a9f1208a771
Rodolfo S Alexandre <r.santos.13@hotmail.com>
2023-06-01 19:34:10 -0300

    Add getters and setters and change tsconfig

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -6,7 +6,11 @@ export type CategoryProperties = {
 }
 
 export class Category {
-    constructor(public readonly props: CategoryProperties){}
+    constructor(public readonly props: CategoryProperties){
+       this.description = this.props.description;
+       this.props.is_active = this.props.is_active ?? true;
+       this.props.created_at = this.props.created_at ?? new Date();
+    }
 
     get name() {
         return this.props.name
@@ -15,13 +19,22 @@ export class Category {
     get description() {
         return this.props.description
     }
+
+    private set description(value: string) {
+        this.props.description = value ?? null;
+    }
     
     get is_active() {
         return this.props.is_active
     }
+
+    private set is_active(value: boolean) {
+        this.props.is_active = value ?? true;
+    }
     
     get created_at() {
         return this.props.created_at
-    }    
+    }
+
     
 }
