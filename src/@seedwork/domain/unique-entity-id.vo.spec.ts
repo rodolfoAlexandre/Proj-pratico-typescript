febc2890f45ff496bb1a77ae463b3b92c3e4e5d0
Rodolfo S Alexandre <r.santos.13@hotmail.com>
2023-06-18 11:39:33 -0300

    test sucess value object

--- src/@seedwork/domain/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -1,5 +1,6 @@
 import InvalidUuidError from "../errors/invalid-uuid.error"
 import UniqueEntityId from "./unique-entity-id.vo"
+import { validate as uuidValidade} from 'uuid'
 
 describe('UniqueEntityId Unit Tests', () => {
     it('Should throw error when uuid is invalid', () => {
@@ -7,4 +8,19 @@ describe('UniqueEntityId Unit Tests', () => {
         expect(() => new UniqueEntityId('fake id')).toThrowError(new InvalidUuidError());
         expect(validateSpy).toHaveBeenCalled()
     });
+
+    it('should accept a uuid passed in constructor', () => {
+        const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+        const uuid = '1656adde-ac6d-43f9-b38f-4cfc796da126'
+        const vo = new UniqueEntityId(uuid);
+        expect(vo.id).toBe(uuid)
+        expect(validateSpy).toHaveBeenCalled()
+    })
+
+    it('should accept a uuid passed in constructor', () => {
+        const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+        const vo = new UniqueEntityId();
+        expect(uuidValidade(vo.id)).toBeTruthy()
+        expect(validateSpy).toHaveBeenCalled()
+    })    
 })
