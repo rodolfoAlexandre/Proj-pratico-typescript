ce57b7bb2b9073667f184cbbeade825598f70fb3
Rodolfo S Alexandre <r.santos.13@hotmail.com>
2023-06-27 18:52:27 -0300

    Add tests for toString method to value object

--- src/@seedwork/domain/value-objects/__tests__/value-object.spec.ts
+++ src/@seedwork/domain/value-objects/__tests__/value-object.spec.ts
@@ -1,8 +1,6 @@
 import ValueObject from "../value-object";
 
-class StubValueObject extends ValueObject{
-
-}
+class StubValueObject extends ValueObject{}
 
 describe('ValueObject Unit Tests', () => {
     it('should set value', () => {
@@ -11,5 +9,28 @@ describe('ValueObject Unit Tests', () => {
 
         vo = new StubValueObject({prop1: 'value1'})
         expect(vo.value).toStrictEqual({prop1: 'value1'})
+    })
+
+    it('should convert to a string', () => {
+        const date = new Date();
+        let arrange = [
+            { recived: null, expected: "null" },
+            { recived: undefined, expected: "undefined" },
+            { recived: "", expected: "" },
+            { recived: "fake test", expected: "fake test" },
+            { recived: 0, expected: "0" },
+            { recived: 1, expected: "1" },
+            { recived: 5, expected: "5" },
+            { recived: true, expected: "true" },
+            { recived: false, expected: "false" },
+            { recived: date, expected: date.toString() },
+            { recived: { prop1: 'value1' }, expected: JSON.stringify({ prop1: 'value1' }) },
+        ]
+
+        arrange.forEach(value => {
+            const vo = new StubValueObject(value.recived)
+            expect(vo + "").toBe(value.expected)  
+        });
+
     })
 })
